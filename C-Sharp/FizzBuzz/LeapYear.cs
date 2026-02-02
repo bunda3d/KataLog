0052fa77ff2f6bf898bd93cbf4ce37e33650c86e
bunda3d <bunda3@yahoo.com>
2026-02-01 20:12:54 -0600

    Leap Year - init commit

---
+++ C-Sharp/FizzBuzz/LeapYear.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzz;
+
+public class LeapYear
+{
+	public bool IsLeapYear(int year)
+	{
+		bool isLeapYear = false;
+
+		//if (year % 4 == 0) return isLeapYear = true;
+
+		return isLeapYear;
+	}
+}
---
+++ C-Sharp/FizzBuzz/LeapYearTests.cs
@@ -0,0 +1,25 @@
+﻿namespace FizzBuzz;
+
+public class LeapYearTests
+{
+	/*
+	Write a function that checks if a year is a leap year.
+	1.	Years divisible by 4 are leap years.
+	2.	EXCEPT years divisible by 100 are NOT leap years.
+	3.	UNLESS years divisible by 400 ARE leap years.
+	*/
+
+	[Theory]
+	[InlineData(1920, true)]
+	public void Divider_ReturnsCorrectString_PerDivisibility(int year, bool isLeapYear)
+	{
+		// Given
+		var leapYear = new LeapYear();
+
+		// When
+		var result = leapYear.IsLeapYear(year);
+
+		// Then
+		Assert.Equal(isLeapYear, result);
+	}
+}
